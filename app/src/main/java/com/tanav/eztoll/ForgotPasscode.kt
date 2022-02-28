1fc591a543d1085501a34260b88acb31c0c2651e
tanavsharma <sharma.tanav95@gmail.com>
2022-02-28 11:20:13 -0500

    Updated the UI for User Information

--- app/src/main/java/com/tanav/eztoll/ForgotPasscode.kt
+++ app/src/main/java/com/tanav/eztoll/ForgotPasscode.kt
@@ -3,12 +3,16 @@ package com.tanav.eztoll
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_forgot_passcode.*
 
 class ForgotPasscode : AppCompatActivity() {
 
     private lateinit var emailAddressET: EditText
     private lateinit var email: String
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,12 +20,13 @@ class ForgotPasscode : AppCompatActivity() {
 
         emailAddressET = findViewById(R.id.emailAddress)
         email = emailAddressET.text.toString()
+        auth = Firebase.auth
 
         forgotPasscodeBtn.setOnClickListener {
-            Firebase.auth.sendPasswordResetEmail(email)
+            auth.sendPasswordResetEmail(email)
                 .addOnCompleteListener { task ->
                     if (task.isSuccessful) {
-                        Log.d(TAG, "Email sent.")
+                        //Log.d(TAG, "Email sent.")
                     }
                 }
         }
